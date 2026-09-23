35655267d50536a342ce74833de544cc0a96a589
Nishanth <imnishanth23@outlook.com>
2026-09-23 19:37:50 +0530

    Phase 3: AKS (B2s_v2 x2) + ACR with AcrPull

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,7 @@ terraform {
   required_version = ">= 1.6"
   required_providers {
     azurerm = { source = "hashicorp/azurerm", version = "~> 4.0" }
+    random  = { source = "hashicorp/random", version = "~> 3.6" }
   }
 }
 
@@ -10,9 +11,83 @@ provider "azurerm" {
   subscription_id = var.subscription_id
 }
 
-variable "subscription_id" { type = string }
+variable "subscription_id" {
+  type = string
+}
+
+variable "location" {
+  type    = string
+  default = "centralindia"
+}
+
+variable "node_vm_size" {
+  type    = string
+  default = "Standard_B2s_v2" # 2 vCPU / 8 GB; 2 nodes = full 4-vCPU Bsv2 quota
+}
 
 resource "azurerm_resource_group" "rg" {
   name     = "rg-bluegreen-demo"
-  location = "centralindia"
+  location = var.location
+}
+
+resource "random_string" "suffix" {
+  length  = 6
+  upper   = false
+  special = false
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "acrbluegreen${random_string.suffix.result}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = false
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aks-bluegreen-demo"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "aksbluegreen"
+  sku_tier            = "Free"
+
+  default_node_pool {
+    name            = "system"
+    vm_size         = var.node_vm_size
+    node_count      = 2
+    os_disk_size_gb = 30
+
+    upgrade_settings {
+      max_surge = "10%"
+    }
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin      = "azure"
+    network_plugin_mode = "overlay"
+    load_balancer_sku   = "standard"
+  }
+}
+
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  scope                            = azurerm_container_registry.acr.id
+  role_definition_name             = "AcrPull"
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  skip_service_principal_aad_check = true
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "acr_name" {
+  value = azurerm_container_registry.acr.name
+}
+
+output "get_credentials_cmd" {
+  value = "az aks get-credentials -g ${azurerm_resource_group.rg.name} -n ${azurerm_kubernetes_cluster.aks.name} --overwrite-existing"
 }
